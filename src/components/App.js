ee2e9cf92fb4cf11ab2fa463fc25ee6a74a5fe07
IlyaMelnichenko <124725419+IlyaMelnichenko@users.noreply.github.com>
2023-08-06 23:03:06 +0300

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,8 @@
 import { Profile } from "./profile/Profile";
-import profileUsers from '../user.json';
-import data from '../data.json'
-import friends from '../friends.json'
-import transactions from '../transactions.json'
+import profileUsers from "../user.json";
+import data from "../data.json"
+import friends from "../friends.json"
+import transactions from "../transactions.json"
 import { Statistics } from "./Statistics/Statistics";
 import { FriendList } from "./FriendList/FriendList";
 import { TransactionHistory } from "./TransactionHistory/TransactionHistory";
@@ -11,13 +11,15 @@ import { GlobalStyle } from "./Globalstyle";
 
 
 export const App = () => {
-  return (<div>
+  return (
+    <>
     <Profile items={profileUsers} />
     <Statistics tittle="Upload stats" stats={data} />
     <FriendList friends={friends} />
     <TransactionHistory items={transactions} />
     <GlobalStyle />
-  </div>);
+    </>
+  );
           
   
 };
